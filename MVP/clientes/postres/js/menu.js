6dbfe0dc3543a18548ad93fb076c474f3339a1da
AlasthorC <davakvdesigner@gmail.com>
2026-07-26 11:26:26 -0600

    detalles extras de fuentes y ortografia

--- MVP/clientes/postres/js/menu.js
+++ MVP/clientes/postres/js/menu.js
@@ -8,19 +8,19 @@ const DATOS_CLIENTE = {
     {
       nombre: "Gelatinas",
       productos: [
-        { id: "gel-fresa", nombreCorto: "Fresa", nombre: "Gelatina de Fresa", precio: 25 },
-        { id: "gel-limon", nombreCorto: "Limón", nombre: "Gelatina de Limón", precio: 25 },
-        { id: "gel-uva", nombreCorto: "Uva", nombre: "Gelatina de Uva", precio: 25 },
-        { id: "gel-pina", nombreCorto: "Piña", nombre: "Gelatina de Piña", precio: 25 },
-        { id: "gel-mosaico", nombreCorto: "Mosaico", nombre: "Gelatina de Mosaico", precio: 30 },
+        { id: "gel-fresa", nombreCorto: "Fresa", nombre: "Gelatina de fresa", precio: 25 },
+        { id: "gel-limon", nombreCorto: "Limón", nombre: "Gelatina de limón", precio: 25 },
+        { id: "gel-uva", nombreCorto: "Uva", nombre: "Gelatina de uva", precio: 25 },
+        { id: "gel-pina", nombreCorto: "Piña", nombre: "Gelatina de piña", precio: 25 },
+        { id: "gel-mosaico", nombreCorto: "Mosaico", nombre: "Gelatina de mosaico", precio: 30 },
         { id: "flan", nombreCorto: "Flan", nombre: "Flan", precio: 35 }
       ]
     },
     {
       nombre: "Con fruta",
       productos: [
-        { id: "pay-limon", nombre: "Pay de Limón", precio: 40 },
-        { id: "pay-mango", nombre: "Pay de Mango", precio: 45 },
+        { id: "pay-limon", nombre: "Pay de limón", precio: 40 },
+        { id: "pay-mango", nombre: "Pay de mango", precio: 45 },
         { id: "fresas-crema", nombre: "Fresas con crema", precio: 50 }
       ]
     },
